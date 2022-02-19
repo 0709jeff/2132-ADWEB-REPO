3b5b07b5b2b0d2e85b35b523273022e742116dad
0709jeff <71444641+0709jeff@users.noreply.github.com>
2022-02-19 15:41:02 +0800

    Resume Project
    ADWEB

---
+++ Resume Project/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'portfolio', component:PortfolioComponent},
+  {path:'skills', component:SkillsComponent},
+  {path:'home', component:HomeComponent},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
